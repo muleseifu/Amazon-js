fc768cac42cf14ea422e15b44b4f072e0959a7a7
muleseifu <muleseifu87@gmail.com>
2025-08-02 10:36:18 +0300

    save carr in localStorage

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -107,7 +107,7 @@ document.querySelectorAll('.js-delete-link').forEach((link) =>{
     const container = document.querySelector(
       `.js-cart-item-container-${productId}`
     )
-    container.remove()
+    container.remove
 
   })
 
