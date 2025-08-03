c5ee244d833ab0a93a28e87db45ed2a7448a4b55
muleseifu <muleseifu87@gmail.com>
2025-08-03 19:40:41 +0300

    Convert products into classes

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "../scripts/checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import '../data/cart-class.js'; //Runs all the code inside given path but do not import what's inside
+//import '../data/cart-class.js'; //Runs all the code inside given path but do not import what's inside
 
 renderOrderSummary();
 renderPaymentSummary();
