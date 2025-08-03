7369623cd746139ce8193e090e15ef0cc149d0a2
muleseifu <muleseifu87@gmail.com>
2025-08-03 22:19:15 +0300

    Load products from backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,9 @@
 import { renderOrderSummary } from "../scripts/checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import { loadProducts } from "../data/products.js";
 //import '../data/cart-class.js'; //Runs all the code inside given path but do not import what's inside
-import '../data/backend-practice.js'
-
-renderOrderSummary();
-renderPaymentSummary();
+//import '../data/backend-practice.js'
+loadProducts(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+})
