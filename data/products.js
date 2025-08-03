eaa218b46c25ceb6db2c4c5792b04927a64006ce
muleseifu <muleseifu87@gmail.com>
2025-08-03 23:04:35 +0300

    Some absurd things changed but never used

--- data/products.js
+++ data/products.js
@@ -61,6 +61,32 @@ class Clothing extends Product{
 
 export let products = []
 
+export function loadProductsFetch() {
+  const promise = fetch(
+    'https://supersimplebackend.dev/products').then((response) =>{
+      return response.json();
+  }).then((productsData)=>{
+    productsData.map((productDetails)=>{
+  if (productDetails.type === 'clothing'){
+    return new Clothing(productDetails)
+  }
+  return new Product(productDetails)
+
+});
+console.log('load products')
+
+
+  })
+  return promise;
+
+}
+/*
+loadProductsFetch().then(()=>{
+  console.log('Next Step')
+})
+  */
+
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "../scripts/checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
 //import '../data/cart-class.js'; //Runs all the code inside given path but do not import what's inside
 //import '../data/backend-practice.js'
 new Promise((resolve)=>{
