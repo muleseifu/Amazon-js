f86e70973c0beec336630e55d454484d1f2c5a91
muleseifu <muleseifu87@gmail.com>
2025-08-03 22:54:52 +0300

    Learn how to convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,7 +3,19 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
 //import '../data/cart-class.js'; //Runs all the code inside given path but do not import what's inside
 //import '../data/backend-practice.js'
+new Promise((resolve)=>{
+    loadProducts(()=>{
+        resolve()
+    });
+}).then(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+
+})
+/*
 loadProducts(()=>{
     renderOrderSummary();
     renderPaymentSummary();
 })
+
+*/
